57b9f2d900b767482a21880fc265e3b5969425bd
Bruno Staine <brunostaine@hotmail.com>
2024-04-18 16:38:10 -0300

    Adicionando um CRUD

---
+++ src/main/java/br/com/brunostaine/padroesprojetospring/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,33 @@
+package br.com.brunostaine.padroesprojetospring.service.impl;
+
+import br.com.brunostaine.padroesprojetospring.model.Cliente;
+import br.com.brunostaine.padroesprojetospring.service.ClienteService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
